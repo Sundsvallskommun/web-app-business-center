20d4474300e4ac478329dbd80026a13ecdd53440
Tim Lind <tim.lind@sundsvall.se>
2024-08-06 12:53:51 +0200

    fix: removed unused code & context variables, created resetDefaults function

--- frontend/.jest/__mocks__/context.mock.js
+++ frontend/.jest/__mocks__/context.mock.js
@@ -1,29 +1,9 @@
 import * as AppContext from '@contexts/app.context';
-import { representingEntity, user, userMeta, cases, notificationAlerts, invoices } from './data.mock.js';
+import { representingEntity } from './data.mock.js';
 
 export const contextValues = {
-  isLoggedIn: true,
-  setIsLoggedIn: jest.fn(),
-  isLoadingCases: false,
-  setIsLoadingCases: jest.fn(),
   representingEntity: representingEntity,
   setRepresentingEntity: jest.fn(),
-  invoices: invoices,
-  setInvoices: jest.fn(),
-  user: user,
-  setUser: jest.fn(),
-  userMeta: userMeta,
-  setUserMeta: jest.fn(),
-  cases: cases,
-  setCases: jest.fn(),
-  changedCases: cases.cases,
-  setChangedCases: jest.fn(),
-  highlightedTableRow: {},
-  setHighlightedTableRow: jest.fn(),
-  notificationAlerts: notificationAlerts,
-  setNotificationAlerts: jest.fn(),
-  isCookieConsentOpen: true,
-  setIsCookieConsentOpen: jest.fn(),
 };
 
 jest.spyOn(AppContext, 'useAppContext').mockImplementation(() => contextValues);
--- frontend/src/app/logout/page.tsx
+++ frontend/src/app/logout/page.tsx
@@ -1,33 +1,16 @@
 'use client';
 
 import { useAppContext } from '@contexts/app.context';
-import { emptyCaseList } from '@services/case-service';
-import { emptyOrganisationInfo } from '@services/organisation-service';
-import { emptyUser } from '@services/user-service';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import { appURL } from '../../utils/app-url';
 
 export default function Logout() {
-  const {
-    setIsLoadingCases,
-    setChangedCases,
-    setUser,
-    setRepresentingEntity,
-    setCases,
-    setHighlightedTableRow,
-    setNotificationAlerts,
-  } = useAppContext();
+  const { resetContextDefaults } = useAppContext();
   const router = useRouter();
 
   useEffect(() => {
-    setIsLoadingCases(true);
-    setChangedCases([]);
-    setUser(emptyUser);
-    setRepresentingEntity(emptyOrganisationInfo);
-    setCases(emptyCaseList);
-    setHighlightedTableRow({});
-    setNotificationAlerts([]);
+    resetContextDefaults();
     localStorage.clear();
     router.push(`${process.env.NEXT_PUBLIC_API_URL}/saml/logout?successRedirect=${`${appURL()}/login?loggedout`}`); // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);

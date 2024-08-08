9012f1f1d0c76d0bd1662c303a22eb56b663d028
Tim Lind <tim.lind@sundsvall.se>
2024-08-08 15:28:15 +0200

    feat: added form logic and adjusted view to design

--- frontend/src/components/content-card/content-card.tsx
+++ frontend/src/components/content-card/content-card.tsx
@@ -1,11 +1,28 @@
-import { Card } from '@sk-web-gui/react';
+import { Card, Divider } from '@sk-web-gui/react';
 
 export const ContentCard: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return (
     <Card className="block">
-      <Card.Body className="w-full pt-24 px-24 pb-32">{children}</Card.Body>
+      <Card.Body className="w-full p-0">{children}</Card.Body>
     </Card>
   );
 };
 
 export default ContentCard;
+
+export const ContentCardHeader: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+  return (
+    <>
+      <div className="flex justify-between items-center py-16 px-24">{children}</div>
+      <Divider className="my-0" />
+    </>
+  );
+};
+
+export const ContentCardBody: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+  return <div className="p-24"> {children}</div>;
+};
+
+export const ContactDetails: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+  return <div className="flex flex-wrap gap-x-[12rem] gap-y-40"> {children}</div>;
+};
--- frontend/src/components/mypages-sections/profile/profile.component.tsx
+++ frontend/src/components/mypages-sections/profile/profile.component.tsx
@@ -1,9 +1,11 @@
-import { Button, Divider, Icon } from '@sk-web-gui/react';
+import { Button, Icon } from '@sk-web-gui/react';
 import _ from 'lodash';
+import { useState } from 'react';
 import { ClientContactSetting } from '../../../interfaces/contactsettings';
 import { useApi } from '../../../services/api-service';
-import ContentCard from '../../content-card/content-card';
-import { ContactDetailContentBox } from './components/contact-detail-content-box.component';
+import ContentCard, { ContactDetails, ContentCardBody, ContentCardHeader } from '../../content-card/content-card';
+import { FormBox } from '../../form/form-box.component';
+import ContactDetailFormLogic from './components/contact-detail-form-logic.component';
 
 const EmptyField = (text: string) => {
   return <span className="italic">{text}</span>;
@@ -19,30 +21,53 @@ const getAddress = (address) => {
 
 export const ProfilePrivate = () => {
   const { data: contactsettings } = useApi<ClientContactSetting>({ url: '/contactsettings', method: 'get' });
+  const [isEdit, setIsEdit] = useState(false);
+
   return (
     <ContentCard>
-      <h1 className="text-h3 flex items-center gap-md">
-        <Icon size={32} name="user-round" />
-        <span>Kontaktuppgifter</span>
-      </h1>
-      <Divider className="my-lg" />
-      <div className="flex flex-wrap gap-x-[12rem] gap-y-40">
-        <ContactDetailContentBox header="Namn">
-          <div>{contactsettings?.name ?? EmptyField('Inget namn tillagt')}</div>
-        </ContactDetailContentBox>
-        <ContactDetailContentBox header="Adress">
-          <div>{getAddress(contactsettings?.address) ?? EmptyField('Ingen address tillagd')}</div>
-        </ContactDetailContentBox>
-        <ContactDetailContentBox header="E-post">
-          <div>{contactsettings?.email ?? EmptyField('Ingen epost-address tillagd')}</div>
-        </ContactDetailContentBox>
-        <ContactDetailContentBox header="Telefonnummer">
-          {contactsettings?.phone ?? EmptyField('Inget telefonnummer tillagt')}
-        </ContactDetailContentBox>
-      </div>
-      <div className="mt-56">
-        <Button color="vattjom">Redigera Kontaktuppgifter</Button>
-      </div>
+      <ContentCardHeader>
+        <h1 className="text-h4-lg mb-0">
+          <div className="flex items-center gap-md">
+            <span>Kontaktuppgifter</span>
+          </div>
+        </h1>
+        <Button
+          size="md"
+          variant="tertiary"
+          showBackground={false}
+          leftIcon={<Icon name={isEdit ? 'x' : 'pen'} />}
+          onClick={() => setIsEdit((isEdit) => !isEdit)}
+        >
+          {isEdit ? 'Avbryt' : 'Redigera'}
+        </Button>
+      </ContentCardHeader>
+
+      <ContentCardBody>
+        <ContactDetailFormLogic formData={{ email: contactsettings?.email ?? '', phone: contactsettings?.phone ?? '' }}>
+          <ContactDetails>
+            <FormBox header="Namn">
+              <div>{contactsettings?.name ?? EmptyField('Inget namn tillagt')}</div>{' '}
+            </FormBox>
+            <FormBox header="Adress">
+              <div>{getAddress(contactsettings?.address) ?? EmptyField('Ingen address tillagd')}</div>
+            </FormBox>
+            <FormBox name="email" header="E-post" isEdit={isEdit}>
+              <div>{contactsettings?.email ?? EmptyField('Ingen epost-address tillagd')}</div>
+            </FormBox>
+            <FormBox name="phone" header="Telefonnummer" isEdit={isEdit}>
+              <div>{contactsettings?.phone ?? EmptyField('Inget telefonnummer tillagt')}</div>
+            </FormBox>
+          </ContactDetails>
+
+          {isEdit && (
+            <div className="mt-56">
+              <Button type="submit" color="vattjom">
+                Spara
+              </Button>
+            </div>
+          )}
+        </ContactDetailFormLogic>
+      </ContentCardBody>
     </ContentCard>
   );
 };

edaaafd0f9e135c25b3cf34228b988c4c6e4f25d
Tim Lind <tim.lind@sundsvall.se>
2024-08-12 10:36:10 +0200

    feat(frontend): added decicions and documents

--- frontend/src/components/mypages-sections/profile/profile-contact-settings.component.tsx
+++ frontend/src/components/mypages-sections/profile/profile-contact-settings.component.tsx
@@ -1,10 +1,12 @@
-import { Button, Checkbox, FormControl, FormLabel, Icon } from '@sk-web-gui/react';
+import { Button, Checkbox, FormControl, FormHelperText, FormLabel, Icon } from '@sk-web-gui/react';
 import { useState } from 'react';
 import { ClientContactSetting } from '../../../interfaces/contactsettings';
 import { useApi } from '../../../services/api-service';
 import ContentCard, { ContentCardBody, ContentCardHeader } from '../../content-card/content-card';
 import { ConnectForm } from '../../form/connect-form.component';
 import ContactSettingsFormLogic from './components/contact-settings-form-logic.component';
+
+const checkboxDescribedbyTextClasses = 'sk-form-checkbox-label ml-24 pl-8 mt-4';
 
 export const ContactSettings = () => {
   const { data: contactsettings } = useApi<ClientContactSetting>({ url: '/contactsettings', method: 'get' });
@@ -31,18 +33,71 @@ export const ContactSettings = () => {
 
       <ContentCardBody>
         <ContactSettingsFormLogic onSubmitSuccess={() => setIsEdit(false)} formData={contactsettings}>
-          <ConnectForm>
-            {({ register }) => (
-              <FormControl fieldset>
-                <FormLabel className="text-large">Påminnelser och notiser</FormLabel>
-                <Checkbox.Group>
-                  <Checkbox {...register('notifications.phone_disabled')}>Sms</Checkbox>
-                  <Checkbox {...register('notifications.email_disabled')}>E-post</Checkbox>
-                </Checkbox.Group>
-              </FormControl>
-            )}
-          </ConnectForm>
+          <div className="flex flex-col gap-y-40">
+            <ConnectForm>
+              {({ register }) => (
+                <FormControl fieldset>
+                  <FormLabel className="text-large">Påminnelser och notiser</FormLabel>
+                  <Checkbox.Group>
+                    <Checkbox {...register('notifications.phone_disabled')}>Sms</Checkbox>
+                    <Checkbox {...register('notifications.email_disabled')}>E-post</Checkbox>
+                  </Checkbox.Group>
+                </FormControl>
+              )}
+            </ConnectForm>
 
+            <ConnectForm>
+              {({ register }) => (
+                <FormControl fieldset>
+                  <FormLabel className="text-large">Beslut och dokument</FormLabel>
+                  <Checkbox.Group>
+                    <>
+                      <Checkbox
+                        {...register('decicionsAndDocuments.digitalInbox')}
+                        aria-describedby="decicionsAndDocuments.digitalInbox-helptext"
+                      >
+                        <div>Digital brevlåda</div>
+                      </Checkbox>
+                      <FormHelperText
+                        id="decicionsAndDocuments.digitalInbox-helptext"
+                        className={checkboxDescribedbyTextClasses}
+                      >
+                        Brev och fakturor kommer till den digitala brevlådan du är ansluten till.
+                      </FormHelperText>
+                    </>
+                    <>
+                      <Checkbox
+                        {...register('decicionsAndDocuments.myPages')}
+                        aria-describedby="decicionsAndDocuments.myPages-helptext"
+                      >
+                        <div>Mina sidor</div>
+                      </Checkbox>
+                      <FormHelperText
+                        id="decicionsAndDocuments.myPages-helptext"
+                        className={checkboxDescribedbyTextClasses}
+                      >
+                        Vi mejlar dig när det finns något nytt att läsa eller betala.
+                      </FormHelperText>
+                    </>
+                    <>
+                      <Checkbox
+                        {...register('decicionsAndDocuments.snailmail')}
+                        aria-describedby="decicionsAndDocuments.snailmail-helptext"
+                      >
+                        <div>Brev</div>
+                      </Checkbox>
+                      <FormHelperText
+                        id="decicionsAndDocuments.snailmail-helptext"
+                        className={checkboxDescribedbyTextClasses}
+                      >
+                        Brev och fakturor kommer hem till dig i brevlådan.
+                      </FormHelperText>
+                    </>
+                  </Checkbox.Group>
+                </FormControl>
+              )}
+            </ConnectForm>
+          </div>
           {isEdit && (
             <div className="mt-56">
               <Button type="submit" color="vattjom">

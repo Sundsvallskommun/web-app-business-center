983455abb8f3f824f12a26333f9ec851a440fbb0
Tim Lind <tim.lind@sundsvall.se>
2024-08-30 16:21:09 +0200

    fix: adjusting behaviour of representMode switching

--- frontend/src/contexts/app.context.tsx
+++ frontend/src/contexts/app.context.tsx
@@ -38,24 +38,13 @@ export function AppWrapper({ children }) {
   const [representingMode, setRepresentingMode] = useState<RepresentingMode>(defaults.representingMode);
 
   const { setRepresenting } = useRepresentingSwitch();
-  const {
-    data: representingEntity,
-    isLoading: representingIsLoading,
-    isFetching: representingIsFetching,
-  } = useApi<RepresentingEntity>({
-    url: '/representing',
-    method: 'get',
-  });
 
   const switchRepresentingMode = async (newMode: RepresentingMode) => {
-    if (newMode !== representingMode) {
-      setRepresentingMode(newMode);
-      if (newMode !== undefined) {
-        if (getRepresentingMode() !== newMode) {
-          const pathname = newRepresentingModePathname(newMode);
-          router.push(`${appURL()}${pathname}`);
-        }
-      }
+    setRepresentingMode(newMode);
+    const routeRepresentingMode = getRepresentingMode();
+    if (routeRepresentingMode !== null && routeRepresentingMode !== newMode) {
+      const pathname = newRepresentingModePathname(newMode);
+      router.push(`${appURL()}${pathname}`);
       setRepresenting({ mode: newMode });
     }
   };
@@ -63,15 +52,6 @@ export function AppWrapper({ children }) {
   const resetContextDefaults = () => {
     setRepresentingMode(defaults.representingMode);
   };
-
-  useEffect(() => {
-    if (!representingIsLoading && !representingIsFetching) {
-      if (representingEntity?.mode === undefined) {
-        setRepresenting({ mode: defaults.representingMode });
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [representingIsLoading, representingIsFetching]);
 
   return (
     <AppContext.Provider

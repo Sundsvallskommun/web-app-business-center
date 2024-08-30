b5fe0d47265e64707d5ef81259bf6b83b213b2dd
Tim Lind <tim.lind@sundsvall.se>
2024-08-30 13:18:49 +0200

    fix: added missing dependency, removed unused

--- frontend/.jest/__mocks__/context.mock.js
+++
@@ -1,9 +0,0 @@
-import * as AppContext from '@contexts/app.context';
-import { representingEntity } from './data.mock.js';
-
-export const contextValues = {
-  representingEntity: representingEntity,
-  setRepresentingEntity: jest.fn(),
-};
-
-jest.spyOn(AppContext, 'useAppContext').mockImplementation(() => contextValues);

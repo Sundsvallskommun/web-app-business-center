a54a734ca266cda2281fc3e23ae7e39cabb7d958
Tim Lind <tim.lind@sundsvall.se>
2024-09-25 11:41:05 +0200

    fix: centralized styles, mobile adjustments

--- frontend/src/components/mypages-sections/overview/todo/todo-contract.component.tsx
+++ frontend/src/components/mypages-sections/overview/todo/todo-contract.component.tsx
@@ -1,17 +1,22 @@
-import { Button } from '@sk-web-gui/react';
+import { Button, Icon } from '@sk-web-gui/react';
+import styles from './todos.module.scss';
 
 export const TodoContract = () => {
   return (
-    <div className="flex items-center gap-x-md">
-      <div>CONTRACT</div>
-      <div className="grow my-md">
-        <h2 className="text-h4-md">Förnya avtal X</h2>
-        <p>
+    <div className={styles['todo']}>
+      <div className={styles['todo-type']}>
+        <Icon className={styles['todo-type-icon']} name="file-plus-2" />
+      </div>
+      <div className={styles['todo-content']}>
+        <h2 className={styles['todo-content-heading']}>Förnya avtal X</h2>
+        <p className={styles['todo-content-text']}>
           Ditt avtal går ut den <strong>27 april</strong> och behöver förnyas.
         </p>
       </div>
-      <div>
-        <Button color="vattjom">Till avtalet</Button>
+      <div className={styles['todo-action']}>
+        <Button className={styles['todo-action-button']} color="vattjom" aria-label={`AVTAL, till avtalet`}>
+          Till avtalet
+        </Button>
       </div>
     </div>
   );

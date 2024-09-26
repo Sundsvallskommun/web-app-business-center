cf326dadfaf57351d96939615d04611793af8ae8
Tim Lind <tim.lind@sundsvall.se>
2024-09-26 09:59:24 +0200

    feat: added case page routing
    fix: moved pageslayout

--- frontend/src/app/foretag/(mypages-sections)/arenden/[case]/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import Case from '@components/mypages-sections/cases/case/case.component';
-
-export default function Arende({ params }) {
-  return <Case caseId={params.case} />;
-}
--- frontend/src/app/foretag/(mypages-sections)/profil/page.tsx
+++ frontend/src/app/foretag/(mypages-sections)/profil/page.tsx
@@ -1,7 +1,12 @@
 'use client';
 
+import { PagesLayout } from '@layouts/pages-layout.component';
 import { Profile } from '../../../../components/mypages-sections/profile/profile.component';
 
 export default function Profil() {
-  return <Profile />;
+  return (
+    <PagesLayout>
+      <Profile />
+    </PagesLayout>
+  );
 }

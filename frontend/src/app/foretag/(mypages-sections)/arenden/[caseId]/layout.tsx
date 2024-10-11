7edce594697a1e2de5ea85fdd9f9999f4b6df3f0
Tim Lind <tim.lind@sundsvall.se>
2024-10-11 16:21:59 +0200

    fix: refactor of case layout

--- frontend/src/app/foretag/(mypages-sections)/arenden/[caseId]/layout.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import CaseLayout from '@components/mypages-sections/cases/case/case-layout.component';
-
-export default function layout({ params, children }) {
-  return <CaseLayout caseId={params.caseId}>{children}</CaseLayout>;
-}
--- frontend/src/app/privat/arenden/[caseId]/[[...currentTab]]/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import CaseTabLayout from '@components/mypages-sections/cases/case/case-tab-layout.component';
-
-export default function CurrentTab({ params }) {
-  return <CaseTabLayout caseId={params.caseId} currentTab={params.currentTab} />;
-}

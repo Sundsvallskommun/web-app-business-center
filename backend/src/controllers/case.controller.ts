8d1e1633e05ec502fd8331ca2cec3cf6e4595ba4
Tim Lind <tim.lind@sundsvall.se>
2024-10-11 16:19:21 +0200

    fix: adjusted id -> externalCaseId for specific case endpoints

--- backend/src/controllers/case.controller.ts
+++ backend/src/controllers/case.controller.ts
@@ -1,15 +1,14 @@
+import { MUNICIPALITY_ID } from '@/config';
 import { HttpException } from '@/exceptions/HttpException';
 import { RequestWithUser } from '@/interfaces/auth.interface';
 import { Case, CasePdf } from '@/interfaces/case.interface';
 import ApiService from '@/services/api.service';
 import authMiddleware from '@middlewares/auth.middleware';
-import { Controller, Get, Param, QueryParam, Req, UseBefore } from 'routing-controllers';
+import { Controller, Get, Param, Req, UseBefore } from 'routing-controllers';
 import { OpenAPI } from 'routing-controllers-openapi';
 import { RepresentingMode } from '../interfaces/representing.interface';
 import { ApiResponse } from '../interfaces/service';
 import { formatOrgNr } from '../utils/util';
-import { MUNICIPALITY_ID } from '@/config';
-
 @Controller()
 export class CaseController {
   private apiService = new ApiService();
@@ -45,16 +44,16 @@ export class CaseController {
     }
   }
 
-  @Get('/cases/:id')
+  @Get('/cases/:externalCaseId')
   @OpenAPI({ summary: 'Return a case' })
   @UseBefore(authMiddleware)
-  async getCase(@Param('id') id: number): Promise<ApiResponse<Case | null>> {
-    if (!id) {
+  async getCase(@Param('externalCaseId') externalCaseId: number): Promise<ApiResponse<Case | null>> {
+    if (!externalCaseId) {
       throw new HttpException(400, 'Bad Request');
     }
 
     try {
-      const url = `casestatus/3.0/${MUNICIPALITY_ID}/${id}/status`;
+      const url = `casestatus/3.0/${MUNICIPALITY_ID}/${externalCaseId}/status`;
       const res = await this.apiService.get<Case>({ url });
       if (!res.data) {
         return { data: null, message: 'error' };
@@ -66,15 +65,15 @@ export class CaseController {
     }
   }
 
-  @Get('/casepdf/:id')
-  @OpenAPI({ summary: 'Return the base64 encoded pdf by case id' })
+  @Get('/casepdf/:externalCaseId')
+  @OpenAPI({ summary: 'Return the base64 encoded pdf by case externalCaseId' })
   @UseBefore(authMiddleware)
-  async getCasePdf(@Req() req: RequestWithUser, @Param('id') id: string): Promise<ApiResponse<CasePdf>> {
-    if (!id) {
+  async getCasePdf(@Req() req: RequestWithUser, @Param('externalCaseId') externalCaseId: string): Promise<ApiResponse<CasePdf>> {
+    if (!externalCaseId) {
       throw new HttpException(400, 'Bad Request');
     }
 
-    const url = `casestatus/3.0/${MUNICIPALITY_ID}/${id}/pdf`;
+    const url = `casestatus/3.0/${MUNICIPALITY_ID}/${externalCaseId}/pdf`;
     const res = await this.apiService.get<CasePdf>({ url });
 
     return { data: res.data, message: 'success' };

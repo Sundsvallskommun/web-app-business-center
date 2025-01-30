5cc551d33e82fd1846a73ec4c8f1ba6f38487934
Tim Lind <tim.lind@sundsvall.se>
2025-01-30 15:00:37 +0100

    fix: build-fixes from parallell merge miss

--- frontend/src/components/mypages-sections/cases/case/meddelanden/case-message-files.component.tsx
+++ frontend/src/components/mypages-sections/cases/case/meddelanden/case-message-files.component.tsx
@@ -1,11 +1,11 @@
 import { Button, Icon } from '@sk-web-gui/react';
-import { File, Image } from 'lucide-react';
+import { File } from 'lucide-react';
 
 export default function CaseMessageFiles() {
   return (
     <div className="flex gap-x-16">
       {[...Array.from([1, 2, 3])].map((x, index) => {
-        const iconType = <File /> || <Image />; // FIXME: implement filetype check
+        const iconType = <File />; // || <Image />; // FIXME: implement filetype check
         return (
           <Button
             leftIcon={<Icon icon={iconType} />}

a09f7b959c141e1f3ae4d6ca9310c3fe63444cbf
Tim Lind <tim.lind@sundsvall.se>
2025-04-15 14:18:41 +0200

    fix: visual syncs to design

--- frontend/src/layouts/pages-breadcrumbs-layout.component.tsx
+++ frontend/src/layouts/pages-breadcrumbs-layout.component.tsx
@@ -1,6 +1,7 @@
-import { cx } from '@sk-web-gui/react';
+import { cx, useThemeQueries } from '@sk-web-gui/react';
 import { BannerMenu } from './banner-menu/banner-menu.component';
 import MainLayout from './main-layout.component';
+import { usePathname } from 'next/navigation';
 
 export const PagesBreadcrumbsLayout = ({
   children,
@@ -9,9 +10,11 @@ export const PagesBreadcrumbsLayout = ({
   children: React.ReactNode;
   breadcrumbs?: React.ReactNode;
 }) => {
+  const { isMinDesktop } = useThemeQueries();
+  const pathname = usePathname();
   return (
     <>
-      <BannerMenu />
+      {pathname.includes('/oversikt') || isMinDesktop ? <BannerMenu /> : null}
       {breadcrumbs && (
         <div className="max-w-main-content w-full mt-24 desktop:mb-24 px-20 desktop:px-0">{breadcrumbs}</div>
       )}

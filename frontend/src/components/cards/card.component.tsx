ca42d53f772e8dd6a678bf968152e9030969356a
Tim Lind <104623532+timlindsk@users.noreply.github.com>
2025-04-22 09:27:17 +0200

    added background color

--- frontend/src/components/cards/card.component.tsx
+++ frontend/src/components/cards/card.component.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 export const Card = ({ children, className }: CardProps) => {
   return (
     <div
-      className={`border-1 border-divider rounded-cards p-30 desktop:p-32 flex flex-col desktop:gap-y-40, ${className}`}
+      className={`border-1 border-divider rounded-cards p-30 desktop:p-32 flex flex-col desktop:gap-y-40 bg-background-content, ${className}`}
     >
       {children}
     </div>

0b4def3dc462c4ccac6464d7c0ad734978472c5d
ali18rin <alice.ringstrom@sundsvall.se>
2025-08-27 13:43:35 +0200

    fix: profile and settings design update

--- frontend/src/layouts/pages/mypages-sections/profile/profile.component.tsx
+++ frontend/src/layouts/pages/mypages-sections/profile/profile.component.tsx
@@ -1,12 +1,43 @@
+'use client';
+
 import { ContactDetails } from './profile-contact-details.component';
 import { ContactSettings } from './profile-contact-settings.component';
+import { Disclosure } from '@sk-web-gui/react';
 
 export const Profile = () => {
   return (
     <div className="flex flex-col gap-40">
       <h1 className="mb-0">Din profil och inställningar</h1>
-      <ContactDetails />
-      <ContactSettings />
+
+      <Disclosure
+        className="bg-background-content rounded-cards shadow-50 md:py-16 py-32 px-24"
+        data-cy="contact-information-disclosure"
+        header={
+          <>
+            <h4 className="text-h4-md">Kontaktuppgifter</h4>
+            <p className="sm:text-base font-normal mb-0 text-small">
+              Uppdatera dina kontaktuppgifter så att vi kan nå dig.
+            </p>
+          </>
+        }
+      >
+        <ContactDetails />
+      </Disclosure>
+
+      <Disclosure
+        className="bg-background-content rounded-cards shadow-50 md:py-16 py-32 px-24"
+        data-cy="notifications-disclosure"
+        header={
+          <>
+            <h4 className="text-h4-md">Aviseringar</h4>
+            <p className="sm:text-base font-normal mb-0 text-small">
+              Välj hur du vill bli kontaktad när något nytt händer.
+            </p>
+          </>
+        }
+      >
+        <ContactSettings />
+      </Disclosure>
     </div>
   );
 };

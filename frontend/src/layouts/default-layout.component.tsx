3d9ceabf8153f4b57d1df0064403d7d474997160
Max Eriksson <max.z.eriksson@sundsvall.se>
2025-09-02 14:16:11 +0200

    feat: new banner
    - New banner with information and link to old minasidor

--- frontend/src/layouts/default-layout.component.tsx
+++ frontend/src/layouts/default-layout.component.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { Header, useThemeQueries } from '@sk-web-gui/react';
-import { Layout } from './layout.component';
-import { SiteMenu } from './site-menu/site-menu.component';
-import NextLink from 'next/link';
-import { MobileMenu } from './mobile-menu/mobile-menu.component';
 import { appName } from '@utils/app-name';
+import NextLink from 'next/link';
+import { AnnouncementBanner } from './annoncement-banner';
+import { Layout } from './layout.component';
+import { MobileMenu } from './mobile-menu/mobile-menu.component';
+import { SiteMenu } from './site-menu/site-menu.component';
 
 export const DefaultLayout = ({ children }) => {
   const { isMinDesktop } = useThemeQueries();
@@ -13,7 +14,7 @@ export const DefaultLayout = ({ children }) => {
   return (
     <Layout title={`${appName()}`}>
       <Header
-        wrapperClasses="py-16 [&_.sk-header-mobilemenu]:md:block [&_.sk-header-mobilemenu]:desktop:hidden z-[30] shadow-100"
+        wrapperClasses="py-16 [&_.sk-header-mobilemenu]:md:block [&_.sk-header-mobilemenu]:desktop:hidden "
         title={appName()}
         subtitle="Sundsvalls Kommun"
         LogoLinkWrapperComponent={<NextLink href={'/'} legacyBehavior passHref />}
@@ -21,6 +22,9 @@ export const DefaultLayout = ({ children }) => {
       >
         {isMinDesktop && <SiteMenu />}
       </Header>
+
+      <AnnouncementBanner />
+
       {children}
     </Layout>
   );

95a7959328b11ce2da39653a110f469038f491b9
Max Eriksson <max.z.eriksson@sundsvall.se>
2025-09-12 15:15:13 +0200

    feat: new banner text and size

--- frontend/src/layouts/annoncement-banner.tsx
+++ frontend/src/layouts/annoncement-banner.tsx
@@ -8,13 +8,15 @@ const URL_OLD = 'https://e-tjanster.sundsvall.se/minasidor';
 export function AnnouncementBanner() {
   return (
     <div
-      className="w-full bg-background-200 flex items-center justify-center py-10 px-[5rem] min-h-[6rem]"
+      className="w-full bg-background-200 flex items-center justify-center py-10 px-[1.2rem] min-h-[6rem]"
       role="region"
       aria-label="Informationsmeddelande"
     >
       <p className="text-base">
-        Du är nu inloggad i nya Mina sidor. Saknar du något? Du kan byta till den äldre versionen av Mina sidor här.{' '}
-        <Link external href={URL_OLD}>Mina sidor</Link>
+        Du är nu inloggad i nya Mina sidor. Saknar du något kan du fortfarande använda den{' '}
+        <Link external href={URL_OLD}>
+          äldre versionen av Mina sidor
+        </Link>
       </p>
     </div>
   );

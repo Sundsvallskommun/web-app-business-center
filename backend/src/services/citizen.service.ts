c695f0ac056d4fa97e5d34c9f8ce636c962ffd0b
Henrik Sandstrom <henrik.sandstrom@sundsvall.se>
2026-05-21 10:26:06 +0200

    refactor: use citizen service method in more places

--- backend/src/services/citizen.service.ts
+++ backend/src/services/citizen.service.ts
@@ -6,10 +6,23 @@ import { CitizenExtended } from '@/data-contracts/citizen/data-contracts';
 import { User } from '@/interfaces/users.interface';
 
 export const getCitizen = async (partyId: string, req: { user: User }): Promise<CitizenExtended> => {
-  console.log('Fetching citizen', partyId);
   const apiService = new ApiService();
   const citizenApiBase = getApiBase('citizen');
   const citizenUrl = `${citizenApiBase}/${MUNICIPALITY_ID}/${partyId}`;
+  const citizenRes = await apiService.get<CitizenExtended>({ url: citizenUrl }, req.user).catch(() => null);
+
+  if (!citizenRes?.data) {
+    throw new HttpException(500, 'Could not fetch citizen data');
+  }
+
+  const citizen = citizenRes.data;
+  return citizen;
+};
+
+export const getCitizenPersonnumber = async (partyId: string, req: { user: User }): Promise<CitizenExtended> => {
+  const apiService = new ApiService();
+  const citizenApiBase = getApiBase('citizen');
+  const citizenUrl = `${citizenApiBase}/${MUNICIPALITY_ID}/${partyId}/personnumber`;
   const citizenRes = await apiService.get<CitizenExtended>({ url: citizenUrl }, req.user).catch(() => null);
 
   if (!citizenRes?.data) {

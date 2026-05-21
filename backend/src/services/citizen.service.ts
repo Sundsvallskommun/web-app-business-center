e4f4175e79e0fb47048621ff5b27b0a536d07d28
Henrik Sandstrom <henrik.sandstrom@sundsvall.se>
2026-05-21 10:08:41 +0200

    fix: handle differing partyids when mapping sender names

---
+++ backend/src/services/citizen.service.ts
@@ -0,0 +1,21 @@
+import { getApiBase } from '@/config/api-config';
+import ApiService from './api.service';
+import { MUNICIPALITY_ID } from '@/config';
+import { HttpException } from '@/exceptions/HttpException';
+import { CitizenExtended } from '@/data-contracts/citizen/data-contracts';
+import { User } from '@/interfaces/users.interface';
+
+export const getCitizen = async (partyId: string, req: { user: User }): Promise<CitizenExtended> => {
+  console.log('Fetching citizen', partyId);
+  const apiService = new ApiService();
+  const citizenApiBase = getApiBase('citizen');
+  const citizenUrl = `${citizenApiBase}/${MUNICIPALITY_ID}/${partyId}`;
+  const citizenRes = await apiService.get<CitizenExtended>({ url: citizenUrl }, req.user).catch(() => null);
+
+  if (!citizenRes?.data) {
+    throw new HttpException(500, 'Could not fetch citizen data');
+  }
+
+  const citizen = citizenRes.data;
+  return citizen;
+};

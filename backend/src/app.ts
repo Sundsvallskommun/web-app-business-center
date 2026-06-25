b20a6378d745e33cfb3aefacc51013439c17db82
OliverBorgstrom <oliver.borgstrom@sundsvall.se>
2026-06-25 14:06:12 +0200

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -186,6 +186,7 @@ class App {
       logger.info(`======= ENV: ${this.env} =======`);
       logger.info(`🚀 App listening on the port ${this.port}`);
       logger.info(`=================================`);
+      logger.info('ENV KEY', process.env.CLIENT_KEY);
     });
   }
 
